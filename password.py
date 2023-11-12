79769687e7a11094ec7de65f6082d5922c5c0470
Aashutosh Lodhi <66505006+aashutoshlodhi@users.noreply.github.com>
2023-11-12 13:34:09 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,13 +1,17 @@
 import random
 
+// Defining the variables
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number = "0123456789"
 symbol = "`~!@#$%^&*()_-+={[]}<>,./?"
 
+// Combining the string
 string = lower + upper + number + symbol
 length = 20
 
+// Joining String
 password = "".join(random.sample(string, length))
 
+// Print Statement
 print("Your Password is : " + password)
